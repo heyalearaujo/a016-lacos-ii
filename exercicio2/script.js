8035851e6d66ded7dba01b27bace45c280e7ada4
heyalearaujo <alexandrearza@gmail.com>
2022-09-13 16:01:54 -0300

    Resolução Exercicio 2 - Loops 2

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let tabuada = Number(prompt("Digite um número"))
+const numeros = [00,01,02,03,04,05,06,07,08,09,10]
+
+for (let i in numeros) {
+    console.log(`${i} x ${tabuada} = ${numeros
+    *tabuada}`)
+}
